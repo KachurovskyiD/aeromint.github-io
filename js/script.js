a80a8879d4a63bd7f9f76cfab992cb7fba7c76e9
KachurovskyiD <kachurovskyi.d@gmail.com>
2020-11-04 20:27:07 +0200

    trainers block change

--- js/script.js
+++ js/script.js
@@ -152,7 +152,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // Slick slider (trainers block)
 
-  $(window).on('load', function () {
+  $(document).ready(function () {
     $('.trainers__slider-text').slick({
       arrows: false,
       fade: true,
